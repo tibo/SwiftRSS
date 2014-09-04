0f79397738b75706e83f6cc0e0dd9dce474cfea2
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-09-05 01:30:43 +0200

    Initial Commit

---
+++ SwiftRSS_ExampleTests/SwiftRSS_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftRSS_ExampleTests.swift
+//  SwiftRSS_ExampleTests
+//
+//  Created by Thibaut LE LEVIER on 05/09/2014.
+//  Copyright (c) 2014 Thibaut LE LEVIER. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftRSS_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
